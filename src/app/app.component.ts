44e11b5ebf1fcbab8b03ae6611af75e536bf1a5f
aparzi <angeloparziale94@gmail.com>
2018-05-25 17:50:20 +0200

    recupero lista telefoni

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Device} from "../model/device";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+    title = 'app';
+    devices: Device[];
+
+    constructor(private http: HttpClient) {
+        this.getAllDevices();
+    }
+
+    private getAllDevices() {
+        this.http.get<Device[]>('http://localhost:3000/devices')
+            .subscribe(result => this.devices = result);
+    }
 }
